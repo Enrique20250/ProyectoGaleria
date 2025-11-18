ff3c4af9164d2e35e112779aab46087df85c07d3
Enrique20250 <enrique_98_78@hotmail.com>
2025-11-18 11:58:24 -0500

    Imagen

---
+++ backend/models/imagen.php
@@ -0,0 +1,16 @@
+<?php
+class Imagen {
+  
+    private $id;
+    private $titulo;
+    private $descripcion;
+    private $url;
+    private $categoria;
+    private $fechaSubida;
+  
+    public function obtenerPorId() {}
+    public function buscarPorCategoria() {}
+    public function buscarPorTexto() {}
+    public function guardar() {}
+}
+?>
