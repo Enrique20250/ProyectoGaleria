94e8eb46ea5a1410dc71e8a820c1a558747d126f
Enrique20250 <enrique_98_78@hotmail.com>
2025-11-18 12:00:00 -0500

    favorito

---
+++ backend/models/favorito.php
@@ -0,0 +1,9 @@
+<?php
+class Favorito {
+    private $idUsuario;
+    private $idImagen;
+    public function marcar() {}
+    public function desmarcar() {}
+    public function obtenerFavoritos() {}
+}
+?>
